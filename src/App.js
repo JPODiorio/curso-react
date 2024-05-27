ac51d6a32b31270390ae6f2d898543b399b2ad45
João Pedro <jpodiorio@gmail.com>
2024-05-26 23:49:19 -0300

    alterações até a aula 4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './components/HelloWorld';
 
 function App() {
+
+  const name = 'João Pedro'
+  
+  const newName = name.toUpperCase();
+
+  const url = 'https://via.placeholder.com/150'
+
+  function soma(a, b){
+    return a+b
+  }
+
   return (
     <div className="App">
-      <p>meu primeiro projeto</p>
+      <h2>Alterando o JSX</h2>
+      <p>Olá, {newName}</p>
+      <p>Soma: {soma(3, 8)}</p>
+      <img src={url} alt = "imagem"/>
+      <HelloWorld />
     </div>
   );
 }
---
+++ src/components/Frase.js
@@ -0,0 +1,7 @@
+function Frase(){
+    return(
+        <p>Isso é uma frase</p>
+    )
+}
+
+export default Frase
